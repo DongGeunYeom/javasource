1333e7a10e55abac6d4731d560b4cd76ddf38b07
yeomdonggeun <meronot@naver.com>
2022-03-28 16:43:46 +0900

    owner 삭제

--- gittest/src/test/Account.java
+++ gittest/src/test/Account.java
@@ -1,5 +1,14 @@
 package test;
 
-public class Account {
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Setter
+@Getter
+@ToString
+public class Account {
+	private String ano;
+	private int balance;
+	
 }
